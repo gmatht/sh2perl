4cc33c5b404f8ddf6d8bcdd0b3bf5d9cd43f0477
gmatht <gmatht@gmail.com>
2025-09-18 01:07:08 +0800

    Avoid examples with OS dependant output and otherwise non-reproducable.

--- examples/000__05_system_utilities.sh
+++ examples/000__05_system_utilities.sh
@@ -14,19 +14,19 @@ echo "Formatted date: $formatted_date"
 
 # time command with backticks
 #PERL_MUST_NOT_CONTAIN `time
-time_result=`time sleep 1 2>&1`
+time_result=`(time sleep 1) 2>&1 | sed s/...$//`
 echo "Time result: $time_result"
 
 # sleep command with backticks (though it doesn't produce output)
 #PERL_MUST_NOT_CONTAIN `sleep
-sleep_duration=`echo "2"`
-echo "Sleeping for $sleep_duration seconds..."
-sleep $sleep_duration
+#sleep_duration=`echo "2"`
+#echo "Sleeping for $sleep_duration seconds..."
+#sleep $sleep_duration
 
 # which command with backticks
 #PERL_MUST_NOT_CONTAIN `which
-bash_path=`which bash`
-echo "Bash path: $bash_path"
+#bash_path=`which bash`
+#echo "Bash path: $bash_path"
 
 # yes command with backticks
 #PERL_MUST_NOT_CONTAIN `yes
