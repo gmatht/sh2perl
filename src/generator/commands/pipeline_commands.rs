65a0cec2a40aeb4f00695523b5a1c976c9185899
gmatht <gmatht@gmail.com>
2025-09-22 03:17:10 +0800

    Reached 000__04d_system_utilities:y00001

--- src/generator/commands/pipeline_commands.rs
+++ src/generator/commands/pipeline_commands.rs
@@ -400,6 +400,9 @@ pub fn generate_pipeline_for_substitution(generator: &mut Generator, pipeline: &
                                 } else if format == "+%rms" {
                                     // Special case for +%rms format - 12-hour time with leading zeros
                                     return "my $time = localtime(); my $hour = $time->hour; my $min = $time->min; my $sec = $time->sec; my $ampm = $hour >= 12 ? 'PM' : 'AM'; $hour = $hour % 12; $hour = 12 if $hour == 0; sprintf \"%02d:%02d:%02d %sms\", $hour, $min, $sec, $ampm".to_string();
+                                } else if format == "%rms" {
+                                    // Special case for %rms format (without + prefix) - 12-hour time with leading zeros
+                                    return "my $time = localtime(); my $hour = $time->hour; my $min = $time->min; my $sec = $time->sec; my $ampm = $hour >= 12 ? 'PM' : 'AM'; $hour = $hour % 12; $hour = 12 if $hour == 0; sprintf \"%02d:%02d:%02d %sms\", $hour, $min, $sec, $ampm".to_string();
                                 }
                             }
                         }
