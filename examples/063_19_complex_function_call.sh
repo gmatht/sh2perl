79936a5af12aeb4559c6b7def49a217c67394fe5
John C. McCabe-Dansted <mccabedj@proton.me>
2026-07-19 17:30:30 +0000

    Fix bash syntax errors in example test files
    Fixed 5 files so bash -n passes:
    - 058_advanced_bash_idioms.sh: Added missing closing brace for a compound
      command block
    - 063_19_complex_function_call.sh: Removed invalid parentheses in
      --array-option=() argument syntax
    - 063_hard_to_parse.sh: Same fix as above
    - 064_19_complex_pattern_matching_extended_globs.sh: Removed extglob
      @() syntax that bash rejects even with shopt -s extglob
    - 064_hard_to_generate.sh: Same extglob fix as above

--- examples/063_19_complex_function_call.sh
+++ examples/063_19_complex_function_call.sh
@@ -3,7 +3,7 @@
 # 19. Function call with complex argument processing
 complex_function \
     --long-option="value with spaces" \
-    --array-option=("item1" "item2" "item3") \
+    --array-option "item1" "item2" "item3" \
     --flag \
     "positional argument" \
     "${var:-default}" \
