ee4dc29db33a4b0faec07f9a77b267b2e1189866
John McCabe-Dansted <gmatht@gmail.com>
2026-07-20 12:42:54 +0000

    Test results:  passed,  failed (fixed 35)

--- examples.out/test_system_builtin.sh.pl
+++ examples.out/test_system_builtin.sh.pl
@@ -13,24 +13,24 @@ our $CHILD_ERROR;
 
 $PROGRAM_NAME = 'test_system_builtin.sh';
 print "Testing " . "sys" . "tem" . " calls with builtin commands\n";
+my $result2 = do { my $command = q{find . -name '*.txt'}; my $result = qx{$command}; $CHILD_ERROR = $? >> 8; $result; };
 my $result1 = do {
-    my @ls_files_343 = ();
+    my @ls_files_345 = ();
     if ( -f q{.} ) {
-        push @ls_files_343, q{.};
+        push @ls_files_345, q{.};
     }
     elsif ( -d q{.} ) {
         if ( opendir my $dh, q{.} ) {
             while ( my $file = readdir $dh ) {
-                push @ls_files_343, $file;
+                push @ls_files_345, $file;
             }
             closedir $dh;
-            @ls_files_343 = map { $_->[0] } sort { $a->[1] cmp $b->[1] } map { [ $_, do { (my $s = $_) =~ s{/$}{}msx; $s } ] } @ls_files_343;
+            @ls_files_345 = map { $_->[0] } sort { $a->[1] cmp $b->[1] } map { [ $_, do { (my $s = $_) =~ s{/$}{}msx; $s } ] } @ls_files_345;
         }
     }
-    @ls_files_343 = map { my $isdir = (-d $_ || -d ( q{.} . q{/} . $_ )); ($isdir ? 'd ' : '- ') . $_ } @ls_files_343;
-    (@ls_files_343 ? join("\n", @ls_files_343) . "\n" : q{});
+    @ls_files_345 = map { my $isdir = (-d $_ || -d ( q{.} . q{/} . $_ )); ($isdir ? 'd ' : '- ') . $_ } @ls_files_345;
+    (@ls_files_345 ? join("\n", @ls_files_345) . "\n" : q{});
 };
-my $result2 = do { my $command = q{find . -name '*.txt'}; my $result = qx{$command}; $CHILD_ERROR = $? >> 8; $result; };
 print "Results:\n";
 print $result1;
 if ( !( ($result1) =~ m{\n\z}msx ) ) { print "\n"; }
