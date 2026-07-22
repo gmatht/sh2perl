bb55e2d633fdb82ce9aa21e09504268d8c78486c
John McCabe-Dansted <gmatht@gmail.com>
2026-07-22 01:50:33 +0000

    Test results: 140 passed, 3 failed (kept despite regression)

--- examples.out/010_pattern_matching.sh.pl
+++ examples.out/010_pattern_matching.sh.pl
@@ -12,18 +12,18 @@ my $__set_e        = 0;
 our $CHILD_ERROR;
 
 $PROGRAM_NAME = '010_pattern_matching.sh';
-my $f1;
-my @f1;
-my %f1;
-my $word;
-my @word;
-my %word;
-my $f2;
-my @f2;
-my %f2;
 my $s;
 my @s;
 my %s;
+my $f1;
+my @f1;
+my %f1;
+my $f2;
+my @f2;
+my %f2;
+my $word;
+my @word;
+my %word;
 
 $__set_e = 1;
 # set uo not implemented
