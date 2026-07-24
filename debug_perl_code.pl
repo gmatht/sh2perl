c64e3be2497720309d8104d1b6db60eedde8365e
John McCabe-Dansted <gmatht@gmail.com>
2026-07-24 05:37:03 +0000

    Test results: 165 passed, 4 failed (kept despite regression)

--- debug_perl_code.pl
+++ debug_perl_code.pl
@@ -12,28 +12,66 @@ my $__set_e        = 0;
 my $output         = q{};
 our $CHILD_ERROR;
 
-$PROGRAM_NAME = 'test_system_builtin.sh';
-print "Testing " . "sys" . "tem" . " calls with builtin commands\n";
-my $result1;
-my @result1;
-my %result1;
-$result1 = do { my @_qx_cmd = ('ls -la'); my $result = qx{$_qx_cmd[0]}; $CHILD_ERROR = $? >> 8; $result; };
-my $result2;
-my @result2;
-my %result2;
-$result2 = do {
-    require File::Find;
-    my @find_results;
-    File::Find::find(sub { if ($_ =~ /^.*\.txt$/msx) { push @find_results, $File::Find::name; } }, q{.});
-    my $result = join "\n", @find_results;
-    if ($result ne q{}) { $result .= "\n"; }
-    $CHILD_ERROR = 0;
-    $result;
-};
-print "Results:\n";
-print $result1;
-if ( !( ($result1) =~ m{\n\z}msx ) ) { print "\n"; }
-print $result2;
-if ( !( ($result2) =~ m{\n\z}msx ) ) { print "\n"; }
+$PROGRAM_NAME = '087_function_cmd_sub.sh';
+
+sub upper {
+    my $val;
+    $val = do { local $CHILD_ERROR = 0; my $_pipeline_result = do {
+    my $input_data = ("$_[0]") . "\n";
+    my $set1_406 = 'a-z';
+my $set2_406 = 'A-Z';
+my $input_406 = $input_data;
+# Expand character ranges for tr command
+my $expanded_set1_406 = $set1_406;
+my $expanded_set2_406 = $set2_406;
+# Handle a-z range in set1
+if ($expanded_set1_406 =~ /a-z/msx) {
+    $expanded_set1_406 =~ s/a-z/abcdefghijklmnopqrstuvwxyz/msx;
+}
+# Handle A-Z range in set1
+if ($expanded_set1_406 =~ /A-Z/msx) {
+    $expanded_set1_406 =~ s/A-Z/ABCDEFGHIJKLMNOPQRSTUVWXYZ/msx;
+}
+# Handle [:upper:] POSIX class in set1
+if ($expanded_set1_406 =~ /\[:upper:\]/msx) {
+    $expanded_set1_406 =~ s/\[:upper:\]/ABCDEFGHIJKLMNOPQRSTUVWXYZ/msx;
+}
+# Handle [:lower:] POSIX class in set1
+if ($expanded_set1_406 =~ /\[:lower:\]/msx) {
+    $expanded_set1_406 =~ s/\[:lower:\]/abcdefghijklmnopqrstuvwxyz/msx;
+}
+# Handle a-z range in set2
+if ($expanded_set2_406 =~ /a-z/msx) {
+    $expanded_set2_406 =~ s/a-z/abcdefghijklmnopqrstuvwxyz/msx;
+}
+# Handle A-Z range in set2
+if ($expanded_set2_406 =~ /A-Z/msx) {
+    $expanded_set2_406 =~ s/A-Z/ABCDEFGHIJKLMNOPQRSTUVWXYZ/msx;
+}
+# Handle [:upper:] POSIX class in set2
+if ($expanded_set2_406 =~ /\[:upper:\]/msx) {
+    $expanded_set2_406 =~ s/\[:upper:\]/ABCDEFGHIJKLMNOPQRSTUVWXYZ/msx;
+}
+# Handle [:lower:] POSIX class in set2
+if ($expanded_set2_406 =~ /\[:lower:\]/msx) {
+    $expanded_set2_406 =~ s/\[:lower:\]/abcdefghijklmnopqrstuvwxyz/msx;
+}
+my $tr_result_405 = q{};
+for my $char ( split //msx, $input_406 ) {
+    my $pos_406 = index $expanded_set1_406, $char;
+    if ( $pos_406 >= 0 && $pos_406 < length $expanded_set2_406 ) {
+        $tr_result_405 .= substr $expanded_set2_406, $pos_406, 1;
+    } else {
+        $tr_result_405 .= $char;
+    }
+}
+$tr_result_405
+}; $_pipeline_result; };
+    print $val;
+if ( !( ($val) =~ m{\n\z}msx ) ) { print "\n"; }
+    return;
+}
+upper("hello");
+upper("world");
 
 exit $main_exit_code;
