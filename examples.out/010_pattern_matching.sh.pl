f7073cd2647b5625e629864d24f75af60a45ebdd
John McCabe-Dansted <gmatht@gmail.com>
2026-07-24 23:31:59 +0000

    All tests passing

--- examples.out/010_pattern_matching.sh.pl
+++ examples.out/010_pattern_matching.sh.pl
@@ -13,6 +13,9 @@ my $output         = q{};
 our $CHILD_ERROR;
 
 $PROGRAM_NAME = '010_pattern_matching.sh';
+my $f2;
+my @f2;
+my %f2;
 my $s;
 my @s;
 my %s;
@@ -22,9 +25,6 @@ my %word;
 my $f1;
 my @f1;
 my %f1;
-my $f2;
-my @f2;
-my %f2;
 
 $__set_e = 1;
 # set uo not implemented
