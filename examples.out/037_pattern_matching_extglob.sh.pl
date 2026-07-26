3617a47247d5674faa4c56b8e31f221f400df780
John McCabe-Dansted <gmatht@gmail.com>
2026-07-26 12:34:55 +0000

    All tests passing

--- examples.out/037_pattern_matching_extglob.sh.pl
+++ examples.out/037_pattern_matching_extglob.sh.pl
@@ -13,12 +13,12 @@ my $output         = q{};
 our $CHILD_ERROR;
 
 $PROGRAM_NAME = '037_pattern_matching_extglob.sh';
-my $f1;
-my @f1;
-my %f1;
 my $f2;
 my @f2;
 my %f2;
+my $f1;
+my @f1;
+my %f1;
 
 $__set_e = 1;
 # set uo not implemented
