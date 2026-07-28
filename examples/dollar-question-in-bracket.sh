f3644417206c2af8064631e1c1ecb7c6e0156f83
John McCabe-Dansted <gmatht@gmail.com>
2026-07-28 06:33:36 +0000

    Add printf output for undisplayed captured variables in 3 new examples

--- examples/dollar-question-in-bracket.sh
+++ examples/dollar-question-in-bracket.sh
@@ -3,5 +3,6 @@
 # Parse error: "Unexpected token: Question"
 LIBSSL=$(dpkg-query -f '${db:Status-Abbrev}' -W 'libssl1.0.?' 2>&1)
 if [ $? -eq 2 ]; then
+printf 'LIBSSL=[%s]\n' "$LIBSSL"
     echo "error"
 fi
--- examples/parse-paren-close.sh
+++ examples/parse-paren-close.sh
@@ -4,4 +4,7 @@
 a=$(( (1 + 2) * 3 ))
 b=$(( ( (1 + 2) * 3 ) / 2 ))
 c=$(( a + b ))
+printf 'a=[%s]\n' "$a"
+printf 'b=[%s]\n' "$b"
+printf 'c=[%s]\n' "$c"
 echo "$(( a + b + c ))"
