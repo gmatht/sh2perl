b6c03c6b4ba2605f13b4a7e65483195f94e14058
John McCabe-Dansted <gmatht@gmail.com>
2026-07-29 04:25:32 +0000

    gitignore sh2perl-*.pl temp files in sh2perl/ dir (40 more)

--- sh2perl-d7bMBtG0.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env perl
-use strict;
-use warnings;
-use feature 'say';
-
-if ((-x '/usr/share/unattended-upgrades/update-motd-unattended-upgrades')) {
-# Builtin command 'exec' not implemented
-}
