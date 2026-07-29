fbf3cb00310935699c972c1c43688295345939d2
John McCabe-Dansted <gmatht@gmail.com>
2026-07-29 06:00:39 +0000

    Add zstd -dc and gunzip < examples

---
+++ examples/gunzip_example.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+gunzip < /usr/share/man/uk/man1/w.1.gz > /tmp/w.1
+echo "Decompressed man page"
---
+++ examples/zstd_example.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+zstd -dc /usr/lib/firmware/rp2.fw.zst > /tmp/rp2.fw
+echo "Decompressed rp2 firmware"
