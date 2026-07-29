343f6d0191dc26c65e3250d384b8184fdccee863
John McCabe-Dansted <gmatht@gmail.com>
2026-07-29 04:25:32 +0000

    gitignore sh2perl-*.pl temp files in sh2perl/ dir (40 more)

--- sh2perl-jJQ6B8j2.pl
+++
@@ -1,20 +0,0 @@
-#!/usr/bin/env perl
-use strict;
-use warnings;
-use feature 'say';
-use IPC::Open3;
-
-my $output         = q{};
-our $CHILD_ERROR;
-
-$__set_e = 1;
-# set uo not implemented
-# set pipefail not implemented
-say "== Escape sequences ==";
-say 'bell';
-say 'backspace';
-say 'formfeed';
-say "newline\n";
-say "carriage\rreturn";
-say "tab\tseparated";
-say 'verticaltab';
