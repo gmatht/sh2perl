9a85758637aff750edea7c4f18a3ddb8dc4e02b4
John McCabe-Dansted <gmatht@gmail.com>
2026-07-29 06:55:12 +0000

    All tests passing

--- examples.out/readlink_flags.sh.pl
+++ examples.out/readlink_flags.sh.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-my $target_e = do { my @_qx_cmd = ('command readlink -e "$1"'); chomp(my $result = qx{command $_qx_cmd[0]}); $CHILD_ERROR = $? >> 8; $result; };
-my $target_m = do { my @_qx_cmd = ('command readlink -m "$1"'); chomp(my $result = qx{command $_qx_cmd[0]}); $CHILD_ERROR = $? >> 8; $result; };
-my $target_f = do { my @_qx_cmd = ('command readlink -f "$1"'); chomp(my $result = qx{command $_qx_cmd[0]}); $CHILD_ERROR = $? >> 8; $result; };
-print "Canonical (existing): $target_e\n";
-print "Canonical (missing):  $target_m\n";
-print "Canonical (full):     $target_f\n";
+my $existing = do { my @_qx_cmd = ('command readlink -e /usr/bin/vi'); chomp(my $result = qx{command $_qx_cmd[0]}); $CHILD_ERROR = $? >> 8; $result; };
+my $missing = do { my @_qx_cmd = ('command readlink -m /nonexistent/path'); chomp(my $result = qx{command $_qx_cmd[0]}); $CHILD_ERROR = $? >> 8; $result; };
+my $full = do { my @_qx_cmd = ('command readlink -f /usr/bin/python3'); chomp(my $result = qx{command $_qx_cmd[0]}); $CHILD_ERROR = $? >> 8; $result; };
+print "Existing: $existing\n";
+print "Missing:  $missing\n";
+print "Full:     $full\n";
 
