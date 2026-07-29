178523249a755dd7dbe148363ccf8d5f725e152c
John McCabe-Dansted <gmatht@gmail.com>
2026-07-29 22:08:13 +0000

    Test results: 355 passed, 162 failed (kept despite regression)

---
+++ __tmp_run_3339359.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Carp;
+use English qw(-no_match_vars $ERRNO $EVAL_ERROR $INPUT_RECORD_SEPARATOR $OS_ERROR $PROGRAM_NAME);
+use IPC::Open3;
+my $main_exit_code = 0;
+my $output = '';
+our $CHILD_ERROR;
+
+my $myvar;
+
+if (!("${myvar}" ne q{})) {
+        $myvar = "default";
+}
+print "hello $myvar\n";
+
+exit $main_exit_code;
